f1e8c6e166a704e03dc8268cbfcbd558769d59f3
HelgaRun <60192437+HelgaRun@users.noreply.github.com>
2020-03-30 23:00:38 +0000

    Update DayTripsAPI.java
    Setti inn smá af test unitinu  inn í api inn :) hann er ekki full kláraður

--- API/View/DayTripsAPI.java
+++ API/View/DayTripsAPI.java
@@ -14,4 +14,32 @@ public class DayTripsAPI {
         users.searchCustomer("SELECT * FROM users WHERE firstName = '"+name+"'");
         users.searchAll();
     }
+        // erum með tvær breytur, db og users
+        @Before
+        public void setUp(){
+            System.out.println("@Before setup");
+            // viljum hafa lykkjum sem fer í gegnum ef að strengurinn fyrir name er == null
+            // ef að hann er null þá getum við farið í það að uppfæra static breyturnar í það
+            // sem hefur verið að leita að 
+            if(user == null){
+                user = new User(db);
+            }
+            
+            
+        }
+    
+        @After
+        public void tearDown(){
+            // núllstillum aftur gildin sem að var verið að vinna með 
+           
+            
+    
+        }
+    
+        @Test 
+        public void testingSearch(){
+            ...
+            //vantar hérna hvað við verðum með, s.s hvort það sé array eða ehv annað
+            assertEquals("gildin okkar koma hérna inn"); // mun gefa upp hvort að testið virkar eða ekki 
+        }
 }
