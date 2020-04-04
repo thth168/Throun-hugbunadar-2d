a9e3ac897c81a2a5a91895e76484c5c0205ee19e
HelgaRun <60192437+HelgaRun@users.noreply.github.com>
2020-04-04 01:12:15 +0000

    added controller and updates
    added a search controller where the user can search some info, and then I added some text of things that i dont know how to code in tour controller... the tour controller is just a controller that stores all the information about the tours but is not searchable

--- API/Controller/TourController.java
+++ API/Controller/TourController.java
@@ -1,12 +1,17 @@
+// need to connect to a database, but I do not know how..
+// and then we need to import from there
 public class TourController{
-    // need to connect to a database, but I do not know how 
-    
+    /**
+     * number if trips that are in the db
+     */
     public int numOfTrips;
     /**
      * info about linked tours... don´t know if right
      * hehehe
      */
     public String linkedTours;
+
+    // public Database-connection database;
 
 
 
