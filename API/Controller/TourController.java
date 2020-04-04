6b805eb982923357b7608c64bed873873e3c635b
HelgaRun <60192437+HelgaRun@users.noreply.github.com>
2020-04-04 00:53:57 +0000

    Updated TourController and Tour in model

---
+++ API/Controller/TourController.java
@@ -0,0 +1,14 @@
+public class TourController{
+    // need to connect to a database, but I do not know how 
+    
+    public int numOfTrips;
+    /**
+     * info about linked tours... don´t know if right
+     * hehehe
+     */
+    public String linkedTours;
+
+
+
+
+}
