9b6e2736b304db6ccbb104d1ed8e9fdfb187caea
HelgaRun <60192437+HelgaRun@users.noreply.github.com>
2020-04-04 01:13:18 +0000

    searchController

---
+++ API/Controller/SearchController.java
@@ -0,0 +1,9 @@
+// connection to db and imports still needed
+
+public class SearchController{
+    public String tourCategory;
+    public int tourPrice;
+    public int numOfSeatsLeft;
+    public Date tourDate;
+    public String 
+}
