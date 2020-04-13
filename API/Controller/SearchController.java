0407bebe5ec51965b8017f7219378fe1cb1f9428
HelgaRun <60192437+HelgaRun@users.noreply.github.com>
2020-04-13 21:49:11 +0000

    search filters
    shitty job tbh

--- API/Controller/SearchController.java
+++ API/Controller/SearchController.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import javax.xml.stream.Location;
 
 import javafx.event.ActionEvent;
@@ -8,8 +10,8 @@ import sun.applet.Main;
 
 public class SearchController{
     public static Database db = new Database();
-    public String tourCategory;
-    public int tourPrice;
+    public String tourCategory; // √
+    public int tourPrice; // √
     public int numOfSeatsLeft;
     public Date tourDate;
     public Location tourLocation; // not sure if this is the 100% right way with the import above
@@ -18,4 +20,40 @@ public class SearchController{
     public String groupSeatingOnTour; // ??? don't know if we want this here
     public String uniqueEvents; // do we want this here
     public int tourRating;
+    
+
+    //get and set for tour prices
+    public int getTourPrice(){
+        return tourPrice;
+    }
+
+    public void setTourPrice(){
+        SearchController searchController = this;
+        searchController.tourPrice = tourPrice;
+    }
+
+    // get and set tour category
+    public String getTourCat(){
+        return tourCategory;
+    }
+    public void setTourCat(){
+        SearchController searchController = this;
+        searchController.tourCategorye = tourCategory;
+    }
+
+    //get and set tour date
+    public Date getTourDate(){
+        return tourDate;
+    }
+
+    public void setTourDate(){
+        this.tourDate = tourDate;
+    }
+
+    //get and set number of seats left
+
+    public int getNumOfSeatsLeft(){
+        return numOfSeatsLeft;
+    }
+
 }
