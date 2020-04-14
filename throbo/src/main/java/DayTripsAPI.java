244a39b968ed34596c3c222795d3f74f09c97153
thth168 <43797464+thth168@users.noreply.github.com>
2020-04-14 21:59:08 +0000

    Smá vinna á database
    Creation of vapelord Snorri

--- throbo/src/main/java/DayTripsAPI.java
+++ throbo/src/main/java/DayTripsAPI.java
@@ -1,23 +1,35 @@
 import java.util.Scanner;
+import java.sql.ResultSet;
 import com.hi.d.throbo.Database.Database;
 import com.hi.d.throbo.Model.Users;
+import java.sql.SQLException;
 
 public class DayTripsAPI {
     private static Database db;
     private static Users users;
     
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Scanner s = new Scanner(System.in);
         try {
             db = Database.getInstance();
-            users = new Users();
-            System.out.println("Search User");
-            Scanner s = new Scanner(System.in);
-            String name = s.nextLine();
-            users.searchCustomer("SELECT * FROM users WHERE firstName LIKE '"+name+"'");
-            //users.searchAll();
         } catch(ClassNotFoundException e) {
             System.err.print(e.toString());
         }
+        
+        // byðja um username
+        System.out.println("Type in username:");
+        String username = s.next();
+        // byðja um password
+        System.out.println("Type in password:");
+        String password = s.next();
+        
+        ResultSet rs = db.search("SELECT * FROM users WHERE username = '" + username + "';");
+        if(!rs.getString("password").equals(password)){
+            db.close();
+            System.out.println("Invalid credentials!!!");
+            return;
+        }
+        System.out.println("Welcome back " + rs.getString("firstName"));
         db.close();
     }
 }
